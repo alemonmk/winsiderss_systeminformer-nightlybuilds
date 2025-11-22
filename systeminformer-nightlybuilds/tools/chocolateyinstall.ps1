a22dc1e532a74b7d775bb3d16ff4dd0995dd6f6b
Lemon Lam <alemonmk@users.noreply.github.com>
2025-11-22 13:02:38 +0800

    Update to 3.2.25324.532
    - Install to package directory instead
    - Use builtin functionality to install for 32/64bit arch
    - Enable non admin user to save program options
    - Fixed package params implementation

--- systeminformer-nightlybuilds/tools/chocolateyinstall.ps1
+++ systeminformer-nightlybuilds/tools/chocolateyinstall.ps1
@@ -1,38 +1,41 @@
-﻿$PackageSourceUrl = "https://github.com/winsiderss/si-builds/releases/download/3.2.25180.1655/systeminformer-3.2.25180.1655-bin.zip"
-$Architecture = if (Get-OSArchitectureWidth 32) { "i386" } else { "amd64" }
-$InstallDirectory = "$(if (Get-OSArchitectureWidth 64) { $Env:ProgramW6432 } else { $Env:ProgramFiles })\SystemInformer"
-
-$PackageParams = @{
+﻿$PackageParams = @{
   PackageName    = "systeminformer-nightlybuilds"
-  UnzipLocation  = $InstallDirectory
-  Url            = $PackageSourceUrl
-  Checksum       = "65f336ca5c9e7001449ea34f035bc9263f997112f1e87bfc59a44ae8cd5bceae"
+  UnzipLocation  = "$(Split-Path $MyInvocation.MyCommand.Definition)\SystemInformer"
+  Url            = "https://github.com/winsiderss/si-builds/releases/download/3.2.25324.532/systeminformer-build-win32-bin.zip"
+  Url64bit       = "https://github.com/winsiderss/si-builds/releases/download/3.2.25324.532/systeminformer-build-win64-bin.zip"
+  Checksum       = "85209032835ef59c14c1b7ac467242a94ccec953b3ce270a1ecab953eb7cff1d"
   ChecksumType   = "sha256"
-  SpecificFolder = $Architecture
+  Checksum64     = "b34582e3c7e227231efc7dfae1ddc8d097493046bb85b6be3735b26e5e809b4e"
+  Checksum64Type = "sha256"
 }
 
-Install-ChocolateyZipPackage @PackageParams
+$InstallDirectory = Install-ChocolateyZipPackage @PackageParams
+if (Get-OSArchitectureWidth 64) { Remove-Item "$InstallDirectory\x86" -Recurse | Out-Null }
 
 # Restore backed up setting file
 $SettingFile = "$Env:TEMP\SystemInformer.exe.settings.xml"
 if (Test-Path $SettingFile) {
-  Copy-Item -Path $SettingFile -Destination "$InstallDirectory\$Architecture" | Out-Null
+  Copy-Item -Path $SettingFile -Destination "$InstallDirectory" | Out-Null
 }
+
+# Grant access to install folder and setting file to builtin users
+$ModifyRight = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Users", "Modify", "ContainerInherit, ObjectInherit", "None", "Allow")
+$Acl = Get-Acl -Path $InstallDirectory
+$Acl.SetAccessRule($ModifyRight)
+Set-Acl -Path $InstallDirectory -AclObject $Acl
 
 $PackageParams = Get-PackageParameters
 
-switch ($PackageParams) {
-  "shortcut" {
-    $ShortcutParams = @{
-      ShortcutFilePath = "$Env:PUBLIC\Desktop\System Informer.lnk"
-      TargetPath       = "$InstallDirectory\$Architecture\SystemInformer.exe"
-      IconLocation     = "$InstallDirectory\$Architecture\SystemInformer.exe"
-    }
-    Install-ChocolateyShortcut @ShortcutParams
+if ($PackageParams["shortcut"]) {
+  $ShortcutParams = @{
+    ShortcutFilePath = "$Env:PUBLIC\Desktop\System Informer.lnk"
+    TargetPath       = "$InstallDirectory\SystemInformer.exe"
+    IconLocation     = "$InstallDirectory\SystemInformer.exe"
   }
-  "defaultTaskMgr" {
-    $RegPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe"
-    if (-not $(Test-Path $RegPath)) { New-Item $RegPath }
-    New-ItemProperty -Path $RegPath -Name "Debugger" -PropertyType "String" -Value "$InstallDirectory\$Architecture\SystemInformer.exe"
-  }
+  Install-ChocolateyShortcut @ShortcutParams
+}
+if ($PackageParams["defaultTaskMgr"]) {
+  $RegPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe"
+  if (-not $(Test-Path $RegPath)) { New-Item $RegPath | Out-Null }
+  New-ItemProperty -Path $RegPath -Name "Debugger" -PropertyType "String" -Value "$InstallDirectory\SystemInformer.exe" -Force | Out-Null
 }
--- systeminformer-nightlybuilds/update.ps1
+++ systeminformer-nightlybuilds/update.ps1
@@ -2,12 +2,11 @@ Import-Module chocolatey-au
 
 function global:au_SearchReplace {
     @{
-        'tools\chocolateyinstall.ps1'   = @{
-            "(\t*PackageSourceUrl\s*=\s*)(`".*`")" = "`$1`"$($Latest.Url32)`""
-            "(\t*Checksum\s*=\s*)(`".*`")"         = "`$1`"$($Latest.Checksum32)`""
-        }
-        'tools\chocolateyuninstall.ps1' = @{
-            "systeminformer-.*?-bin\.zip" = "$($Latest.Filename)"
+        'tools\chocolateyinstall.ps1' = @{
+            "(\t*Url\s*=\s*)(`".*`")"        = "`$1`"$($Latest.Url32)`""
+            "(\t*Url64bit\s*=\s*)(`".*`")"   = "`$1`"$($Latest.Url64)`""
+            "(\t*Checksum\s*=\s*)(`".*`")"   = "`$1`"$($Latest.Checksum32)`""
+            "(\t*Checksum64\s*=\s*)(`".*`")" = "`$1`"$($Latest.Checksum64)`""
         }
     }
 }
@@ -15,14 +14,16 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $releaseInfo = Invoke-RestMethod -Method Get -Uri "https://api.github.com/repos/winsiderss/si-builds/releases/latest"
 
-    $asset = $releaseInfo.assets | Where-Object { $_.name -eq "systeminformer-$($releaseInfo.tag_name)-bin.zip" }
+    $win32asset = $releaseInfo.assets | Where-Object { $_.name -eq "systeminformer-build-win32-bin.zip" }
+    $win64asset = $releaseInfo.assets | Where-Object { $_.name -eq "systeminformer-build-win64-bin.zip" }
 
     $Latest = @{
         Filename = $asset.name
         Version  = $releaseInfo.tag_name
-        Url32    = $asset.browser_download_url
+        Url32    = $win32asset.browser_download_url
+        Url64    = $win64asset.browser_download_url
     }
     return $Latest
 }
 
-update
+update -ChecksumFor all
